085c78ea8bec07300f35053b8a1427c69a111330
Mateusz Jaworski <mateuszjaworski@Mateuszs-MacBook-Air.local>
2025-11-18 18:08:13 +0100

    Added models for movie, director and movie-director relation

--- server/src/common/models.py
+++ server/src/common/models.py
@@ -20,6 +20,32 @@ class User(SQLModel, table=True):
     created_at: datetime = Field(default_factory=datetime.now)
 
 
+class Movie(SQLModel, table=True):
+    __tablename__ = "movies"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(nullable=False)
+    description: str
+    duration: int
+    poster_url: Optional[str] = None
+    release_date: datetime
+    created_at: datetime = Field(default_factory=datetime.now)
+
+class Director(SQLModel, table=True):
+    __tablename__ = "directors"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    first_name: str
+    surname: str
+    picture_url: str
+
+class MovieDirector(SQLModel, table=True):
+    __tablename__ = "MovieDirector"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    movie_id: int
+    director: int
+
 # --------------------
 # Auth Token
 # --------------------
