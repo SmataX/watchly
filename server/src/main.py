e5cd5337716f02df78a28c8c542f84e0c5d54c40
Mateusz Jaworski <mateuszjaworski@Mateuszs-MacBook-Air.local>
2025-11-19 15:56:29 +0100

    Implemented movies module for adding, getting, updating nad deleting movies in db

--- server/src/main.py
+++ server/src/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from src.common.db import create_db_and_tables
 from src.modules.auth import auth_router
+from src.modules.movies import movies_router
 
 
 @asynccontextmanager
@@ -14,6 +15,7 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(auth_router)
+app.include_router(movies_router)
 
 @app.get("/")
 async def root():
