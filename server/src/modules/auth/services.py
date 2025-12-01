87e5c130ef6eac9d4abc50f8c97cb9ea97422626
Mateusz Jaworski <mateuszjaworski@Mateuszs-MacBook-Air.local>
2025-12-01 22:21:55 +0100

    Displaying user info on profile page

--- server/src/modules/auth/services.py
+++ server/src/modules/auth/services.py
@@ -161,7 +161,7 @@ def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]) -> Dict[str,
     )
     
     try:
-        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[config.ALGORITHM])
+        payload = jwt.decode(token, config.SECRET_KEY, algorithms=[config.ALGORITHM])
         username: str = payload.get('sub')
         user_id: int = payload.get('id')
         
