159b1c0e8e0813c8b9c90bf9a5af7ec0c6a3b90f
KaroSigma <Karola9873@gmail.com>
2026-01-13 12:54:01 +0100

    Add favorite movies web and to edit informations

--- client/src/routers/profile.py
+++ client/src/routers/profile.py
@@ -1,3 +1,5 @@
+﻿
+from fastapi.responses import HTMLResponse
 import httpx
 
 from typing import Optional
@@ -20,7 +22,7 @@ async def user_profile(
     client: httpx.AsyncClient = Depends(get_http_client),
 ):
     if not user:
-        return RedirectResponse("/login")
+        return RedirectResponse("http://127.0.0.1:8001/login")
     
     user_data = await get_data(f"/user/{user['username']}", client)
 
@@ -31,6 +33,42 @@ async def user_profile(
     })
 
 
+@router.get("/{username}/favorites")
+async def user_favorites(
+    username: str,
+    request: Request, 
+    client: httpx.AsyncClient = Depends(get_http_client),
+    user: Optional[dict] = Depends(get_optional_user),
+):
+    
+    token = request.cookies.get("access_token")
+    token = token.split(' ')[1] if token else None
+    # Pobiera jakieś filmy z bazy (tymczasowo)
+    movies_data = await get_data("http://127.0.0.1:8001/movies?limit=12", client, token)
+    
+    return templates.TemplateResponse("favorite_movies.html", {
+        "request": request,
+        "user": user,
+        "profile_username": username,
+        "data": movies_data  
+    })
+
+
+
+
+@router.get("/edit", response_class=HTMLResponse)
+async def edit_profile_page(
+    request: Request,
+    user: Optional[dict] = Depends(get_optional_user)
+):
+    if not user:
+        return RedirectResponse(url="/login") 
+
+    return templates.TemplateResponse("edit_profile.html", {
+        "request": request,
+        "user": user
+    })
+
 @router.get("/{username}")
 async def user_profile(
     request: Request, 
@@ -38,7 +76,7 @@ async def user_profile(
     client: httpx.AsyncClient = Depends(get_http_client),
     username: str = ""
 ):
-    user_data = await get_data(f"/user/{username}", client)
+    user_data = await get_data(f"http://127.0.0.1:8001/user/{username}", client)
 
     return templates.TemplateResponse("profile.html", {
         "request": request, 
